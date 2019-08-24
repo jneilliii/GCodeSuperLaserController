4e88e2011d22fa412c5339911df62cf90cacabcd
Skiepp <skiepp93@hotmail.it>
2019-08-24 18:18:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Skiepp"
 plugin_author_email = "skiepp93@hotmail.it"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "TODO"
+plugin_url = "https://github.com/Skiepp/GCodeSuperLaserController"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "Apache License 2.0"
